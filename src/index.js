d732cbf9242dae9dd79c196fb73be435a9e5e807
Steve Doty <stevedoty16@gmail.com>
2020-09-11 13:37:24 -0700

    create store and rootReducer

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
 import {createStore} from 'redux'
-// import rootReducer from './reducers'
+import rootReducer from './reducers'
 
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-
-      <App />
-    
-  </React.StrictMode>,
+      <Provider store={store}>
+        <App />
+      </Provider>,
   document.getElementById('root')
 );
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,14 @@
 import {combineReducers} from 'redux'
+
+// const todos = (state = [], action) => {
+//   switch (action.type) {
+//     default:
+//       return state
+//   }
+// }
+
+
+export default combineReducers({
+  name:()=>{return"steve"},
+  lastName:()=>{return"doty"}
+})
