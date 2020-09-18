c0852076e7b4f10bff6d4dde3565a40ab6c5dde3
Steve Doty <stevedoty16@gmail.com>
2020-09-17 19:04:34 -0700

    production build

---
+++ docs/precache-manifest.7ee22bed357344283fd7c085fdc090c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9968f4555127b68eecc431a9d9192471",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "0adf8eac9f5be5386269",
+    "url": "/crud-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f1333e1dcc71c8ccf89b",
+    "url": "/crud-app/static/css/main.2cbb2599.chunk.css"
+  },
+  {
+    "revision": "0adf8eac9f5be5386269",
+    "url": "/crud-app/static/js/2.26ec2677.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/crud-app/static/js/2.26ec2677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1333e1dcc71c8ccf89b",
+    "url": "/crud-app/static/js/main.b2ebc49b.chunk.js"
+  },
+  {
+    "revision": "6400a072754635706ea1",
+    "url": "/crud-app/static/js/runtime-main.b590a2b6.js"
+  },
+  {
+    "revision": "d96347d83f92b9c4a41683f40830f09e",
+    "url": "/crud-app/static/media/landscape.d96347d8.jpg"
+  }
+]);
